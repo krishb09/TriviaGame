880ad75a6909a40020be605284ada8d43cbe0e2a
Krish <aalsbhowmick@gmail.com>
2019-07-26 15:52:52 -0400

    adding layout

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+//set array with different questions
+
+var questions = ["What is the sunshine state?", "What is the world's longest river?", 
+"What chess piece can only move diagonally?", "When did the Cold War end?", "Where would you find the Sea of Tranquility?", 
+"What is the capital city of Spain?" ]; 
+
+//answer object to hold the answers for the questions
+var answer = {
+    q1: "Florida",
+    q2: "Amazon", 
+    q3: "Bishop", 
+    q4: "1989",
+    q5: "The Moon", 
+    q6: "Madrid"
+}; 
+
+$(".q1").text("What is the sunshine state?"); 
+$(".q2").text("What is the world's longest river?"); 
+$(".q3").text("What chess piece can only move diagonally?"); 
+$(".q4").text("When did the Cold War end?"); 
+$(".q5").text("Where would you find the Sea of Tranquility?"); 
+$(".q6").text("What is the capital city of Spain?"); 
